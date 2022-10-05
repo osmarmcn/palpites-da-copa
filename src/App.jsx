0df1d839ec5b4808bba0da2c1940428143103cf2
osmar mendes <99884631+osmarmcn@users.noreply.github.com>
2022-10-05 10:22:50 -0300

    ajustando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 
+
+
 function App() {
   
 
   return (
-    <div className="bg-red-500">
-      <h1>titulo</h1>
-      <h3>sub-titulo</h3>
-      <h5>assunto</h5>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda quidem numquam nesciunt temporibus nemo optio maiores! Delectus deleniti sapiente tenetur, dignissimos soluta animi illum aliquid, recusandae distinctio necessitatibus facere magni!</p>
      
-    </div>
+      <div className="container p-6 h-screen bg-red-700 text-white flex-col items:center md:flex-row space-y-6 md:space-y-0 md:space-x-6">
+        <div className="md:flex-1">
+          <img src="/imagem/img.png" alt="" className="w-full"/>
+        </div>
+
+        <div className="md:flex-1 flex flex-col space-y-6">
+          <h1 className="text-3xl text-center text-left font-bold">Dê seu palpite na copa do mundo do Catar 2022!</h1>
+
+          <button className="text-red-700 bg-white text-xl p-10 py-4 rounded-3xl">
+            Criar minha conta
+          </button>
+
+          <button className="text-red-700 border border-white bg-white text-xl p-10 py-4 rounded-3xl">
+            Criar minha conta
+
+          </button>
+        </div>
+      </div>
+    
+    
   )
 }
 
