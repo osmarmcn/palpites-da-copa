77180c2c03a8622b87fb85fae10809206d7019bf
osmar mendes <99884631+osmarmcn@users.noreply.github.com>
2022-10-05 09:08:00 -0300

    primeiro comando

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  
+
+  return (
+    <div className="bg-red-500">
+      <h1>titulo</h1>
+      <h3>sub-titulo</h3>
+      <h5>assunto</h5>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda quidem numquam nesciunt temporibus nemo optio maiores! Delectus deleniti sapiente tenetur, dignissimos soluta animi illum aliquid, recusandae distinctio necessitatibus facere magni!</p>
+     
+    </div>
+  )
+}
+
+export default App
