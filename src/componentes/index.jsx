1c48bf5394578c86dc730eb4449bef1837068336
osmar mendes <99884631+osmarmcn@users.noreply.github.com>
2022-10-07 10:41:29 -0300

    ajustes

--- src/componentes/index.jsx
+++ src/componentes/index.jsx
@@ -1,2 +1,3 @@
 export * from './Icon/Icon'
 export * from './Card'
+export * from './DateSelect'
--- src/paginas/Profile.jsx
+++ src/paginas/Profile.jsx
@@ -1,6 +1,35 @@
-import { Icon, Card } from "../componentes"
 
 
+import { Icon, Card, DateSelect } from "../componentes"
+
+
+const DateSelect = () =>{
+
+    const [currentDate,setCurrentDate] = useState(new Date('2022-11-20T00:00:00'))
+
+    const prevDay = () =>{
+        const nextDate = subDays(currentDate, 1)
+        setCurrentDate(nextDate)
+    }
+
+    const nextDay = () =>{
+        const nextDate = addDays(currentDate, 1)
+        setCurrentDate(nextDate)
+
+    }
+
+
+    return(
+        <div className="p-4 flex space-x-4 justify-center ">
+            <Icon name="arrowLeft" className="w-6 text-red-500" onClick={prevDay}/>
+            <span className="font-bold">{format(currentDate, "d 'de' MMMM", {locale: ptBR})}</span>
+            <Icon name="arrowRight" className="w-6 text-red-500" onClick={nextDay}/>
+
+        </div>
+    )
+    
+
+}
 
 export const Profile = () =>{
     return(
@@ -29,12 +58,7 @@ export const Profile = () =>{
 
                 <h2 className="text-red-500 text-xl font-bold">Seus palpites</h2>
 
-                <div className="p-4 flex space-x-4 justify-center ">
-                    <Icon name="arrowLeft" className="w-6 text-red-500"/>
-                    <span className="font-bold">24 de novembro</span>
-                    <Icon name="arrowRight" className="w-6 text-red-500"/>
-
-                </div>
+                <DateSelect/>
 
                 <div className="space-y-8">
                     <Card
